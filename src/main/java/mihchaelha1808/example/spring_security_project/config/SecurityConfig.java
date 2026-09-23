b8c30f55094bf9d926544c3d35ed1f474bd6658f
Michaelha1808 <haminhchi1808@gmail.com>
2026-09-23 22:05:53 +0700

    add: Roles vs Authorities

--- src/main/java/mihchaelha1808/example/spring_security_project/config/SecurityConfig.java
+++ src/main/java/mihchaelha1808/example/spring_security_project/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -18,17 +20,35 @@ public class SecurityConfig {
      * */
     @Bean
     public UserDetailsService userDetailsService(){
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("222") // raw
+//                .roles("admin","user")
+//                .build();
+//
+//        UserDetails user = User.
+//                 withUsername("user")
+//                .password("{noop}111") // raw
+//                .roles("user")
+//                .build();
+
         UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
-                .password("222") // raw
-                .roles("admin","user")
+                .password(passwordEncoder().encode("222")) // raw
+//                .roles("admin","user")
+                .authorities("ROLE_admin","ROLE_user", "order:read","order:read")
                 .build();
 
         UserDetails user = User.
-                 withUsername("user")
-                .password("{noop}111") // raw
+                withUsername("user")
+                .password(passwordEncoder().encode("111")) // raw
                 .roles("user")
                 .build();
         return new InMemoryUserDetailsManager(admin,user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }

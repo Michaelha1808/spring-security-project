5917cfcd66e02831d3310a8d8ef2e20957b10ef2
Michaelha1808 <haminhchi1808@gmail.com>
2026-09-24 21:04:19 +0700

    add: SecurityFilterChain

--- src/main/java/mihchaelha1808/example/spring_security_project/config/SecurityConfig.java
+++ src/main/java/mihchaelha1808/example/spring_security_project/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package mihchaelha1808.example.spring_security_project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,10 +11,29 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // Active spring web security
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        // tach backend vs frontend
+        http.formLogin((formLogin)-> formLogin.loginProcessingUrl("/login"));
+
+        http.authorizeHttpRequests(req -> req
+                .requestMatchers("/api/v1/auth/login","/api/v1/auth/register")
+                .permitAll()
+//                .requestMatchers("/v1/admin/normal").hasAnyRole("ADMIN","USER")
+//                .requestMatchers("/v1/admin/vip").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+        );
+        return http.build();
+    }
 
     /**
      * Config User information
@@ -20,6 +41,8 @@ public class SecurityConfig {
      * */
     @Bean
     public UserDetailsService userDetailsService(){
+
+
 //        UserDetails admin = User.withDefaultPasswordEncoder()
 //                .username("admin")
 //                .password("222") // raw
@@ -32,17 +55,18 @@ public class SecurityConfig {
 //                .roles("user")
 //                .build();
 
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password(passwordEncoder().encode("222")) // raw
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("123")) // raw
 //                .roles("admin","user")
-                .authorities("ROLE_admin","ROLE_user", "order:read","order:read")
+                .authorities("ROLE_ADMIN","ROLE_USER")
                 .build();
 
         UserDetails user = User.
                 withUsername("user")
-                .password(passwordEncoder().encode("111")) // raw
-                .roles("user")
+                .password(passwordEncoder().encode("123")) // raw
+//                .roles("user")
+                .authorities("ROLE_USER")
                 .build();
         return new InMemoryUserDetailsManager(admin,user);
     }
--- src/main/java/mihchaelha1808/example/spring_security_project/controller/AdminController.java
+++ src/main/java/mihchaelha1808/example/spring_security_project/controller/AdminController.java
@@ -1,13 +1,21 @@
 package mihchaelha1808.example.spring_security_project.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/admin")
+@RolesAllowed("ADMIN")
+
 public class AdminController {
 
+    //@PostAuthorize("hasIpAddress("1111"))
     @GetMapping("/vip")
     public String zoneVip(){
         return "zoneVip";
@@ -16,5 +24,10 @@ public class AdminController {
     @GetMapping("/normal")
     public String zoneNormal(){
         return "zoneNormal";
+    }
+
+    @GetMapping("/info")
+    public  Authentication getInfoUser(){
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 }
